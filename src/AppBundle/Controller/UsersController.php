61ef97cf4b63a65b394640c8919baacb821c7090
Ali Enrique Briceño <alienriquebm@gmail.com>
2016-11-10 00:19:05 -0430

    API Rest Implemented

---
+++ src/AppBundle/Controller/UsersController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UsersController extends Controller
+{
+	/* le indicamos el método http, el nombre de la acción y action para decirle que esto es una acción del controlador */
+    public function getUsersAction()
+    {
+        $data = array("Usuarios" => array(
+        array(
+            "nombre"   => "Víctor",
+            "Apellido" => "Robles"
+        ),
+        array(
+            "nombre"   => "Antonio",
+            "Apellido" => "Martinez"
+        )));
+         
+        return $data;
+    }
+}
